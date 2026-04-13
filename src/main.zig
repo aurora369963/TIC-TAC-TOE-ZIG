0ea3ccff7085107e7ead795a4e5654a8b49f92a0
likhith <likhith@GMAIL.COM>
2026-04-13 14:15:49 +0530

    something

--- src/main.zig
+++ src/main.zig
@@ -1,72 +1,27 @@
-//3*3 grid
-// array of array i.e multi dimension array
-// print which player turn and take inpout row and col
-// intialize all elements to zero first
-// each turn update box
-// check any player made the bingo
-//
-
-const std=@import("std");
-const print=std.debug.print;
+const std = @import("std");
+const TIC_TAC = @import("TIC_TAC");
 
 pub fn main() !void {
-    var board:[3][3]u8 =.{
-      {0,0,0},
-      {0,0,0},
-      {0,0,0}
+    // Prints to stderr, ignoring potential errors.
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try TIC_TAC.bufferedPrint();
+}
+
+test "simple test" {
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(i32) = .empty;
+    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
+    try list.append(gpa, 42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "fuzz example" {
+    const Context = struct {
+        fn testOne(context: @This(), input: []const u8) anyerror!void {
+            _ = context;
+            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        }
     };
-    const stdin=std.io.getStdIn().reader();
-
-
-    const buffer:[2]u8=undefined;
-    var turn:u8=1;
-    while(true){
-        const result=isWon(board);
-
-        if(std.mem.equal(result,1)|| std.mem.equal(result,2))
-       {
-           print("====player {} wins==",.{result});
-           break;
-       }
-
-       if(std.mem.equal(u8,turn,1)){
-           print("PLAYER 1 TURN:::::: ",.{})
-           //turn=turn+1;
-       }
-       if(std.mem.equal(u8,turn,2)){
-           print("PLAYER 2 TURN:::::: ",.{})
-         //  turn=turn-1;
-       }
-       const line=try stdin.readUntilDelimiterOrEof(&buffer,'\n');
-       var input=std.mem.split(u8,line," ");
-       board[input.next()][input.next()]=turn;
-
-
-
-    }
-    pub fn isWon(matrix:*[3][3]u8) u8 {
-        //rows
-      for(0..3) |i| {
-          if(matrix[i][0]!=@as(u8,0)){
-              if(matrix[i][0]==matrix[i][1] and matrix[i][1]==matrix[i][2])
-              {
-                  return matrix[i][0];
-              }
-
-          }
-          //col
-          /   for(0..3) |j| {
-              for(0..3) |i|{
-                  if(matrix[i][j]!=@as(u8,0))
-              }
-              if(matrix[i][0]!=@as(u8,0)){
-                  if(matrix[i][0]==matrix[i][1] and matrix[i][1]==matrix[i][2])
-                  {
-                      return matrix[i][0];
-                  }
-
-              }
-      }
-    }
-
+    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
